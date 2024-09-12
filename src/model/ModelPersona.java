2a4467343b7e78a029cba7925d3e186d4b55c06c
coding-samu <quaresima.1943273@studenti.uniroma1.it>
2024-09-12 13:08:41 +0200

    implementata struttura dei package e creato ModelPersona.java

---
+++ src/model/ModelPersona.java
@@ -0,0 +1,59 @@
+package model;
+
+public class ModelPersona {
+	
+	private String nome;
+	private String cognome;
+	private String indirizzo;
+	private String telefono;
+	private int eta;
+	
+	public ModelPersona(String nome, String cognome, String indirizzo, String telefono, int eta) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.indirizzo = indirizzo;
+		this.telefono = telefono;
+		this.eta = eta;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public String getCognome() {
+		return this.cognome;
+	}
+	
+	public String getIndirizzo() {
+		return this.indirizzo;
+	}
+	
+	public String getTelefono() {
+		return this.telefono;
+	}
+	
+	public int getEta() {
+		return this.eta;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+	
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	public void setEta(int eta) {
+		this.eta = eta;
+	}
+
+}
